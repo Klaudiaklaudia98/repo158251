2c7ee4560660a416f1a4931d15dcdcf64d9ac870
Klaudiaklaudia98 <75248924+Klaudiaklaudia98@users.noreply.github.com>
2021-02-03 11:57:57 +0100

    Update mysin.cpp

--- mysin.cpp
+++ mysin.cpp
@@ -1,1 +1,26 @@
+#include "mysin.h"
+MySin::MySin()
+{
+}
+MySin::MySin(double x)
+{
+}
+MySin::MySin(const MySin &obj)
+{
+}
+MySin::~MySin()
+{
+}
+MySin::double value()
+{
+}
+MySin::void setX(double)
+{
+}
+MySin::double getX()
+{
+}
+MySin::double mX()
+{
+}
 
